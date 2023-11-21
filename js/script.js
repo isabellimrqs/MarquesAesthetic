b4ac946a56912e754c044858a8c42111ccf926fd
Isabelli Marques <isabellimarques14@gmail.com>
2023-11-21 08:14:32 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-export const catalogoProdutos = [
+const catalogoProdutos = [
     {
         codigoProduto: 1,
         nomeProduto:"Jazz Half Ring in 18k Yellow Gold",
@@ -36,40 +36,40 @@ export const catalogoProdutos = [
         categoria:"WEDDING",
         mostrarNaHome: false
     },
-    {
-        codigoProduto: 5,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 559.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"WEDDING",
-        mostrarNaHome: true
-    },
-    {
-        codigoProduto: 6,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 659.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"WEDDING",
-        mostrarNaHome: true
-    },
-    {
-        codigoProduto: 7,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 759.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Home",
-        mostrarNaHome: true
-    },
-    {
-        codigoProduto: 8,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 859.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Home",
-        mostrarNaHome: true
-    },
-]
+//     {
+//         codigoProduto: 5,
+//         nomeProduto:"Kit de Brocas e Beats",
+//         preco: 559.99,
+//         imagemProduto:"./image/ferramentas/kit1.jpg",
+//         descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+//         categoria:"WEDDING",
+//         mostrarNaHome: true
+//     },
+//     {
+//         codigoProduto: 6,
+//         nomeProduto:"Kit de Brocas e Beats",
+//         preco: 659.99,
+//         imagemProduto:"./image/ferramentas/kit1.jpg",
+//         descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+//         categoria:"WEDDING",
+//         mostrarNaHome: true
+//     },
+//     {
+//         codigoProduto: 7,
+//         nomeProduto:"Kit de Brocas e Beats",
+//         preco: 759.99,
+//         imagemProduto:"./image/ferramentas/kit1.jpg",
+//         descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+//         categoria:"Home",
+//         mostrarNaHome: true
+//     },
+//     {
+//         codigoProduto: 8,
+//         nomeProduto:"Kit de Brocas e Beats",
+//         preco: 859.99,
+//         imagemProduto:"./image/ferramentas/kit1.jpg",
+//         descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+//         categoria:"Home",
+//         mostrarNaHome: true
+//     },
+ ]
