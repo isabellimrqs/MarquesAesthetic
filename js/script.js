c4690d689d1ba80d0fc7fed775838d98aa6c6621
Isabelli Marques <isabellimarques14@gmail.com>
2023-11-20 22:57:22 -0300

    Organizando Catálogo Home

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 export const catalogoProdutos = [
     {
         codigoProduto: 1,
-        nomeProduto:"Jazz Half Ring in 18k Yellow Gold",
-        preco: 8190.00,
-        imagemProduto:"./image/products/product6.webp",
+        nomeProduto:"Yellow Gold Half Ring 18k",
+        preco: 850.00,
+        imagemProduto:"./image/products/product1.jpg",
         descricaoProduto:"Stones: Diamond Rhodium: No Average Number of Stones: 11 diamond stones Collection: Half Alliance Eternity Warranty: 12 months Suggestion: For her Material: Yellow gold Code: AN00051898 Height: Approximately 1 mm Note: Due to each monitor and because it is a natural product, The color of the stone may vary slightly from the photo on the website. All measurements are approximate and may vary depending on production.",
         categoria:"WEDDING",
         mostrarNaHome: true
     },
     {
         codigoProduto: 2,
-        nomeProduto:" Yellow Gold Infinity Ring",
+        nomeProduto:"Solitaire Ring Forever White Gold",
         preco: 2550.00,
-        imagemProduto:"./image/products/product6.webp",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+        imagemProduto:"./image/products/product2.jpg",
+        descricaoProduto:"Stones: DiamondSuggestion: For her Center stone: 0.70 Ct Collection: Solitaire Forever Material: White gold Code: AN00045016",
         categoria:"WEDDING",
-        mostrarNaHome: false
+        mostrarNaHome: true
         
     },
     {
         codigoProduto: 3,
-        nomeProduto:"Infinity Silver and Topaz Bracelet",
+        nomeProduto:"Yellow Gold Topaz Sky Diamond Bracelet",
         preco: 550.00,
-        imagemProduto:"./image/products/product5.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"WEDDING",
-        mostrarNaHome: false
+        imagemProduto:"./image/products/product3.jpg",
+        descricaoProduto:"Suggestion: For her Collection: Talisman Warranty: 3 months Material: Silver + Yellow Gold Plating Code: PL00001046 Average Length: Approximately 20 cm Stones: Zirconia ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
+        categoria:"FEMININE",
+        mostrarNaHome: true
     },
     {
         codigoProduto: 4,
-        nomeProduto:"Yellow Gold Half Ring 18k",
-        preco: 850.00,
-        imagemProduto:"./image/products/product1.jpg",
-        descricaoProduto:"Stones: Diamond Rhodium: No Average Number of Stones: 11 diamond stones Collection: Half Alliance Eternity Warranty: 12 months Suggestion: For her Material: Yellow gold Code: AN00051898 Height: Approximately 1 mm Note: Due to each monitor and because it is a natural product, The color of the stone may vary slightly from the photo on the website. All measurements are approximate and may vary depending on production.",
-        categoria:"WEDDING",
-        mostrarNaHome: false
+        nomeProduto:"White Gold Earrings w/ Emeralds and Diamonds",
+        preco: 8627.50,
+        imagemProduto:"./image/products/product4.jpg",
+        descricaoProduto:"Stones: Diamond Rhodium: Yes Average Number of Stones: 4 emeralds and 86 diamonds Suggestion: For her Collection: Classic Warranty: 12 Material: White gold Code: BR00048113: Diamond Rhodium: No Average Number of Stones: 11 diamond stones Collection: Half Alliance Eternity Warranty: 12 months Suggestion: For her Material: Yellow gold Code: AN00051898 Height: Approximately 1 mm Note: Due to each monitor and because it is a natural product, The color of the stone may vary slightly from the photo on the website. All measurements are approximate and may vary depending on production.",
+        categoria:"JEWELS",
+        mostrarNaHome: true
     },
     {
         codigoProduto: 5,
